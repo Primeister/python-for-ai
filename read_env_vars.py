02e1e2cd967d41da7a02ee96b1914738cf5dae27
Primeister <princedlomo21@gmail.com>
2025-11-20 13:12:52 +0200

    added .env file for environment variables

--- read_env_vars.py
+++ read_env_vars.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+#put path in parantheses
+load_dotenv(".env")
 
 api_key = os.environ.get('API_KEY')
 database = os.environ.get('DATABASE')
+
+print(f"api key: {api_key}")
